470c6def232beb4fdc1489663c799d84c0803cf3
Nick Stellato <nick@nickstellato.com>
2015-02-12 07:34:58 -0600

    Changed Twillio to Twilio

--- send_text.py
+++ send_text.py
@@ -8,13 +8,13 @@ config.read('config.cfg')
 
 def main():
 
-    account_sid = config.get('twillio', 'account_sid')
-    auth_token  = config.get('twillio', 'auth_token')
+    account_sid = config.get('twilio', 'account_sid')
+    auth_token  = config.get('twilio', 'auth_token')
     client      = TwilioRestClient(account_sid, auth_token)
 
     message = client.sms.messages.create(
-        body  = "First Message",
-        to    = config.get('message', 'to'),
+        body = "First Message",
+        to   = config.get('message', 'to'),
         from_ = config.get('message', 'from'))
 
 if __name__ == '__main__':
